b4c400aaf9ff84c0fc3b5468a60d7f4d6b0c2bf0
Akansha More <akanshamore21@gmail.com>
2022-09-19 22:16:06 +0530

    Created visibility toggle app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+
 import './App.css';
+import  {useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+
+const[toggle, setToggle] = useState(false)
+
+const handleToggle= ()=> {
+setToggle(!toggle)
+
+}
+  return (
+
+    
+<div>
+
+
+<h1>Visibility Toggle</h1>
+<button onClick= {handleToggle}>{toggle?' Hide details':' show details'}   </button>
+
+
+{toggle ? <div>details are shown. Click to hide the details</div>:null}
+
+
+    </div>
+
+  );
+  
+  
+}
+  export default App;
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
